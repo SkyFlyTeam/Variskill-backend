28663f9283ca2b9bb126b5cc63cfba11e410c386
andresalerno <andresalerno22@gmail.com>
2026-09-15 17:58:40 -0300

    VAR-39 feat: implementar CRUD de atividades com autenticacao JWT

--- settings/settings.py
+++ settings/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'main',
     'userManagement',
+    'learning',
 ]
 
 AUTH_USER_MODEL = 'userManagement.User'
@@ -136,6 +138,18 @@ STATIC_URL = 'static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
+
+# JSON Web Tokens
+# https://django-rest-framework-simplejwt.readthedocs.io/
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
 
