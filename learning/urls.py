c3823bbb2e176ff0c7be55aac6a72cd719a497f7
Ivan Suiyama Silva <ivan.suiya@gmail.com>
2026-09-16 01:56:14 -0300

    VAR-44 feat: implementar CRUD de conteudo

--- learning/urls.py
+++ learning/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import ActivityViewSet
+from .views import ActivityViewSet, ContentViewSet
 
 router = DefaultRouter()
 router.register('atividades', ActivityViewSet, basename='atividade')
+router.register('conteudos', ContentViewSet, basename='conteudo')
 
 urlpatterns = router.urls
