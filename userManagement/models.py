da749826f56a7d9290700fda3979879dd087466c
Eric Lourenço <ericloumendes@gmail.com>
2026-09-16 21:34:10 -0300

    VAR-33 refactor: Adequar entidade User ao esquema do banco de dados feat: Adicionar rota me da entidade User
    No ramo VAR-33-Adequar-entidade-usuario
    Mudanças a serem submetidas:
    - modified:   userManagement/admin.py
    - modified:   userManagement/managers.py
    - new file:   userManagement/migrations/0002_rename_nickname_user_apelido_user_criado_em_and_more.py
    - modified:   userManagement/models.py
    - modified:   userManagement/serializers.py
    - modified:   userManagement/tests/test_views.py
    - modified:   userManagement/views.py

--- userManagement/models.py
+++ userManagement/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -5,12 +6,19 @@ from .managers import UserManager
 
 
 class User(AbstractUser):
-    username = None
-    nickName = models.CharField(max_length=150, unique=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
-    USERNAME_FIELD = 'nickName'
+    username = None
+    apelido = models.CharField(max_length=150, unique=True)
+    nome = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    xp_total = models.IntegerField(default=0)
+    streak_dias = models.IntegerField(default=0)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'apelido'
     REQUIRED_FIELDS = []
     objects = UserManager()
 
     def __str__(self):
-        return self.nickName
+        return self.apelido
