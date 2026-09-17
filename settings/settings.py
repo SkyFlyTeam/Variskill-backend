69586d1f6471ccb9ab2ffad3606c32da7bc39279
andresalerno <andresalerno22@gmail.com>
2026-09-16 21:52:35 -0300

    VAR-57 feat: criar models de intencoes e base vetorial com pgvector

--- settings/settings.py
+++ settings/settings.py
@@ -41,12 +41,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'rest_framework',
     'drf_spectacular',
     'main',
     'userManagement',
     'learning',
     'grading',
+    'intents',
 ]
 
 AUTH_USER_MODEL = 'userManagement.User'
