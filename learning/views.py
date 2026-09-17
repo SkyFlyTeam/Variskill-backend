6420527ef17a52edf83521b8dbb99a52605aaef2
Ivan Suiyama Silva <ivan.suiya@gmail.com>
2026-09-17 13:07:01 -0300

    VAR-44 fix: adaptar CRUD de conteudo para autenticacao por sessao/cookie

--- learning/views.py
+++ learning/views.py
@@ -31,6 +31,7 @@ class ActivityViewSet(viewsets.ModelViewSet):
 
 
 class ContentViewSet(viewsets.ModelViewSet):
+    authentication_classes = (SessionAuthentication,)
     permission_classes = (permissions.IsAuthenticated, IsAdminForUnsafeMethods)
     queryset = Content.objects.all().order_by('id')
     serializer_class = ContentSerializer
