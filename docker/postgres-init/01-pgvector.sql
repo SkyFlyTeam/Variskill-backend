7b3049bd1cc2211a295e673a3fec5cd73ea4a417
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2026-09-16 21:28:09 -0300

    VAR-62 Criar models do app trackManagement

---
+++ docker/postgres-init/01-pgvector.sql
@@ -0,0 +1,2 @@
+-- ensure pgvector extension exists in the database
+CREATE EXTENSION IF NOT EXISTS vector;
---
+++ trackManagement/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class TrackManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'trackManagement'
+    verbose_name = 'Track Management'
