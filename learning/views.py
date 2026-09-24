d67e1f37e3b548741f5f4a7c0f43239b00c14b15
Eric Lourenço <77221715+ericloumendes@users.noreply.github.com>
2026-09-23 22:48:20 -0300

    Revert "VAR-44 refactor: remover extras nao previstos na task"
    This reverts commit a028ddeaabb7c9c06cd6cfcf08f502a0d6ff6743.

--- learning/views.py
+++ learning/views.py
@@ -33,5 +33,5 @@ class ActivityViewSet(viewsets.ModelViewSet):
 class ContentViewSet(viewsets.ModelViewSet):
     authentication_classes = (SessionAuthentication,)
     permission_classes = (permissions.IsAuthenticated, IsAdminForUnsafeMethods)
-    queryset = Content.objects.all()
+    queryset = Content.objects.all().order_by('id')
     serializer_class = ContentSerializer
