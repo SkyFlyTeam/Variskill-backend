7c529e9cc6ff6deed5f990c35199d2af5c74f93a
Eric Lourenço <77221715+ericloumendes@users.noreply.github.com>
2026-09-23 22:48:20 -0300

    Revert "VAR-39 feat: implementar CRUD de atividades com autenticacao JWT"
    This reverts commit 28663f9283ca2b9bb126b5cc63cfba11e410c386.

--- learning/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import ActivityViewSet
-
-router = DefaultRouter()
-router.register('atividades', ActivityViewSet, basename='atividade')
-
-urlpatterns = router.urls
